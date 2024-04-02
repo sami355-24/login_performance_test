eafafc41ab72c84c4de036f50ec32415f0d0bc4e
sami355-24 <ddung355@gmail.com>
2024-04-02 22:51:23 +0900

    [establish](/Login): 롬복 의존성 추가

--- src/main/java/ToyProject/Login/loadgenerator/LoadController.java
+++ src/main/java/ToyProject/Login/loadgenerator/LoadController.java
@@ -1,16 +1,14 @@
 package ToyProject.Login.loadgenerator;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LoadController {
 
     private final LoadService service;
-
-    public LoadController(LoadService service) {
-        this.service = service;
-    }
 
     @GetMapping("/load")
     public void startLoad(){
