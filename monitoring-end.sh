febba77f7ce843190c1390180e4a977053740443
sami355-24 <ddung355@gmail.com>
2024-04-16 17:54:37 +0900

    [refactor]: 스크립트 파일에서 sudo 제거

--- monitoring-end.sh
+++ monitoring-end.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
-
-# Grafana와 Prometheus 프로세스 종료
+#스프링부트, 마리아디비, 프로메테우스, 그라파나를 종료하는 스크립트
 echo "Attempting to kill Grafana and Prometheus processes..."
-sudo pkill -f grafana
-sudo pkill -f prometheus
+pkill -f grafana
+pkill -f prometheus
 echo "Grafana and Prometheus processes have been terminated."
 
-# 특정 포트를 사용하는 프로세스 종료
+# 프로메테우스, 그라파나 종료
 PORTS=(8080 9090)
 for PORT in "${PORTS[@]}"; do
-    PIDS=$(sudo lsof -t -i:$PORT)
+    PIDS=$(lsof -t -i:$PORT)
     for PID in $PIDS; do
         if [ ! -z "$PID" ]; then
             echo "Killing process $PID on port $PORT."
-            sudo kill -9 $PID
+            kill -9 $PID
         else
             echo "No process is using port $PORT."
         fi
@@ -21,7 +19,7 @@ for PORT in "${PORTS[@]}"; do
 done
 echo "All specified processes have been killed."
 
-# Docker Compose 관련 작업 종료
+# 스프링부트 마리아디비 종료
 echo "Shutting down Docker Compose..."
-docker compose down --remove-orphans
+docker compose -f docker-compose-springboot-mariadb.yaml down --remove-orphans
 echo "Docker Compose has been shut down."
