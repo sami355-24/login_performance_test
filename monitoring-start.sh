c7dd860b8dbb63502ec7a3795cd19a709bf9cf6c
sami355-24 <ddung355@gmail.com>
2024-04-16 17:52:43 +0900

    [feat]: 도커 컴포즈 파일명 수정에 따른 쉘 내역 변경

--- monitoring-start.sh
+++ monitoring-start.sh
@@ -1,8 +1,12 @@
-cd /Users/sangmin/Desktop/Login || exit
-docker compose up --build -d
+# 스프링부트, 마리아디비, 프로메테우스, 그라파나를 실행하는 스크립트
+# 스프링부트 빌드
+docker compose -f docker-compose-springboot-mariadb.yaml build --no-cache
+# 스프링부트, 마리아디비 실행
+docker compose -f docker-compose-springboot-mariadb.yaml up -d
 echo "Docker compose started and sleeping for 40 seconds..."
 sleep 40
 
+# 프로메테우스, 그라파나 실행
 cd /Users/sangmin/Local/prometheus-2.51.1.darwin-amd64 || exit
 ./monitoring-prometheus.sh &
 echo "Prometheus started"
