a38651225afb7943d892f5f4ed0b6e9308be677b
sami355-24 <ddung355@gmail.com>
2024-04-19 16:58:06 +0900

    [feat]: 부하 주는 로직의 컨트롤러 수정

--- src/main/java/ToyProject/Login/loadgenerator/LoadController.java
+++ src/main/java/ToyProject/Login/loadgenerator/LoadController.java
@@ -1,17 +1,26 @@
 package ToyProject.Login.loadgenerator;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/load")
 @RequiredArgsConstructor
 public class LoadController {
 
     private final LoadService service;
 
-    @GetMapping("/load")
-    public void startLoad(){
+    @GetMapping("/real")
+    public ResponseEntity<String> startLoad(){
         service.loadGenerator();
+        return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping("/fake")
+    public ResponseEntity<String> startFakeLoad(){
+        return ResponseEntity.ok("ok");
     }
 }
