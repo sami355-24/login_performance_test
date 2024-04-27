c29721b7f25024496574cb239a3f33ae876f074d
sami355-24 <ddung355@gmail.com>
2024-04-27 11:18:25 +0900

    ent

--- src/main/java/ToyProject/Login/member/MemberController.java
+++
@@ -1,9 +0,0 @@
-package ToyProject.Login.member;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "member")
-public class MemberController {
-}
--- src/main/java/ToyProject/Login/member/MemberRepository.java
+++ src/main/java/ToyProject/Login/member/MemberRepository.java
@@ -3,4 +3,6 @@ package ToyProject.Login.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
+
+    boolean existsByEmailAndPassword(String email, String password);
 }
