fd425d321328fb261f47c7b942f7634c25458ff0
sami355-24 <ddung355@gmail.com>
2024-05-15 00:22:34 +0900

    [feat]: 인증 프로세스 중에서 인증 MemberDetails(UserDetails)를 생성해내는 클래스 생성

---
+++ src/main/java/ToyProject/Login/security/userDetails/MemberDetailsService.java
@@ -0,0 +1,32 @@
+package ToyProject.Login.security.userDetails;
+
+import ToyProject.Login.login.LoginDto;
+import ToyProject.Login.member.Member;
+import ToyProject.Login.member.MemberRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member findMember = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("%s 이메일을 지닌 유저가 존재하지 않습니다.", username)));
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(findMember.getRoles()));
+        ModelMapper mapper = new ModelMapper();
+        LoginDto loginDto = mapper.map(findMember, LoginDto.class);
+
+        return new MemberDetails(loginDto, authorities);
+    }
+}
