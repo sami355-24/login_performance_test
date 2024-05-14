0bda169ed609a541cfc5deb5414a142112700f42
sami355-24 <ddung355@gmail.com>
2024-05-15 00:09:58 +0900

    [feat]: 사용자 email로 사용자 찾는 메서드 추가

--- src/main/java/ToyProject/Login/member/MemberRepository.java
+++ src/main/java/ToyProject/Login/member/MemberRepository.java
@@ -1,8 +1,11 @@
 package ToyProject.Login.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
     boolean existsByEmailAndPassword(String email, String password);
+
+    Optional<Member> findByEmail(String email);
 }
