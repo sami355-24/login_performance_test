ff9aeba97d7651f16de71dba4cdd96d0e7040f93
sami355-24 <ddung355@gmail.com>
2024-05-15 00:45:44 +0900

    [feat]: 인증 토큰 생성자 시그니처 수정

--- src/main/java/ToyProject/Login/security/token/RestAuthenticationToken.java
+++ src/main/java/ToyProject/Login/security/token/RestAuthenticationToken.java
@@ -16,7 +16,7 @@ public class RestAuthenticationToken extends AbstractAuthenticationToken {
         setAuthenticated(false);
     }
 
-    public RestAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+    public RestAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
         super(authorities);
         this.principal = principal;
         this.credentials = credentials;
