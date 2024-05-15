1ca6cab91d6f0ca329443f69ad80d79fbd0b5721
sami355-24 <ddung355@gmail.com>
2024-05-15 17:57:15 +0900

    [remove]: 컨트롤러에서 존재하는 로그인 로직 삭제(인증 성공시 성공 핸들러에서 관련 로직 수행하기 때문)

--- src/main/java/ToyProject/Login/login/LoginController.java
+++ src/main/java/ToyProject/Login/login/LoginController.java
@@ -2,7 +2,6 @@ package ToyProject.Login.login;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +13,6 @@ public class LoginController {
 
     public static final String SESSION = "MEMBER_SESSION";
 
-    @PostMapping("/login")
-    public ResponseEntity<Void> Login(@AuthenticationPrincipal LoginDto loginDto){
-        return ResponseEntity.ok().build();
-    }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(){
