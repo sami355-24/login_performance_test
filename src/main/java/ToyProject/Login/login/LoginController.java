d4de60980e704a55d0b44ca2169804aec661a749
sami355-24 <ddung355@gmail.com>
2024-05-21 23:46:19 +0900

    [feat]: Login 컨트롤러의 JWT 발급 받는 메소드 추가

---
+++ src/main/java/ToyProject/Login/login/LoginController.java
@@ -0,0 +1,25 @@
+package ToyProject.Login.login;
+
+
+import ToyProject.Login.member.MemberRepository;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(loginService.issueAccessToken(loginDto));
+    }
+
+}
