dd4b6636c271bc680dd5ae7116470035704c8be5
sami355-24 <ddung355@gmail.com>
2024-05-22 18:40:46 +0900

    [feat]: 스프링 시큐리티 설정 코드 추가

--- src/main/java/ToyProject/Login/security/config/SecurityConfig.java
+++ src/main/java/ToyProject/Login/security/config/SecurityConfig.java
@@ -1,33 +1,53 @@
 package ToyProject.Login.security.config;
 
+import ToyProject.Login.security.filters.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationProvider jwtProvider;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManagerBuilder.class).build();
+
         return http
                 .formLogin(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(
-                        sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/auth/login").permitAll()
-                                .anyRequest().authenticated()
+                                .requestMatchers("load/real", "load/fake").hasRole("USER")
+                                .anyRequest().permitAll()
                 )
+                .addFilterBefore(jwtFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(jwtProvider)
+                .authenticationManager(authenticationManager)
                 .build();
+    }
+
+    private JwtFilter jwtFilter(AuthenticationManager authenticationManager) {
+        JwtFilter jwtFilter = new JwtFilter();
+        jwtFilter.setAuthenticationManager(authenticationManager);
+        return jwtFilter;
     }
 }
