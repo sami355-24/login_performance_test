f08709ed9c380e422be275a09fea4d5f82122ee8
sami355-24 <ddung355@gmail.com>
2024-05-22 18:35:59 +0900

    [feat]: JwtAuthenticationToken 다른 필터에서 사용할수 있도록 속성, 생성자 추가

--- src/main/java/ToyProject/Login/security/token/JwtAuthenticationToken.java
+++ src/main/java/ToyProject/Login/security/token/JwtAuthenticationToken.java
@@ -1,14 +1,29 @@
 package ToyProject.Login.security.token;
 
+import java.util.Collection;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 
 public class JwtAuthenticationToken extends AbstractAuthenticationToken {
 
-    private String authToken;
+    private final String authToken;
+    private final Object principal;
+    private final Object credentials;
 
     public JwtAuthenticationToken(String authToken) {
         super(null);
         this.authToken = authToken;
+        this.principal = null;
+        this.credentials = null;
+        setAuthenticated(false);
+    }
+
+    public JwtAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
+        super(authorities);
+        this.authToken = null;
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true);
     }
 
     @Override
